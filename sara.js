bb3761c98cd7b75257fce8610b737571842f22fc
unknown <Shahdamr246@icloud.com>
2024-09-27 17:48:17 +0300

    Fundamental 1

---
+++ sara.js
@@ -0,0 +1,37 @@
+
+let markMass =78;
+let markHeight=1.69;
+let mBMI=markMass/markHeight**2
+console.log(`mark bmi =${mBMI}`);
+let johnMass =80;
+let johnHeight=1.99;
+let jBMI=johnMass/johnHeight**2
+console.log(`john bmi =${jBMI}`);
+if (mBMI=>jBMI){
+    console.log('mark is bigger than john')
+}
+else if(jBMI=>mBMI){
+    console.log('john is bigger than mark')
+}
+    
+
+
+
+
+
+
+
+
+
+/*console .log(firstname);
+firstname=23
+console.log(firstname)
+firstname=true
+console.log(firstname)
+let x='23'
+let y='12'
+console.log(x-y);
+console.log(x+y);
+console.log(x*y);
+console.log(x%y);
+console.log(x/y);*/
